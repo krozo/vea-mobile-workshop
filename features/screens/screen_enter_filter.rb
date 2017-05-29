9cdd5eaab7a024d4e915d65345a1b52a50091d9b
Krozo <denisrimeiko@gmail.com>
2017-05-29 18:02:33 +0300

    Name variable work correct, now need create with filter

--- features/screens/screen_enter_filter.rb
+++ features/screens/screen_enter_filter.rb
@@ -2,10 +2,15 @@ class ScreenEnterFilter
     def initialize(driver)
         @driver = driver
         @filter_save_button = Element.new(:id, 'save_filter', @driver)
+        @filter_name = Element.new(:id, 'param_filter_name', @driver)
     end
 
     def visible?
         @filter_save_button.visible?
+    end
+
+    def enter_filter_name(name)   
+        @filter_name.send_keys name 
     end
 
     def enter_save_button
