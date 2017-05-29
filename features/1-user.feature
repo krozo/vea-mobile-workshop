a2622908c17f11b9966970e38a2c64ecb014bf99
Krozo <denisrimeiko@gmail.com>
2017-05-29 14:58:52 +0300

    Added all exercises steps

--- features/1-user.feature
+++ features/1-user.feature
@@ -1,5 +1,19 @@
 Feature: 1. Filter feature
 
-  Scenario: Add filter
-    Given I am on intro screen
-    When I create property filter
+  # Scenario: Add filter
+  #   Given I am on intro screen
+  #   When I create property filter
+
+Scenario: “Nekustamie īpašumi” filter - negative
+  Given I am on intro screen
+  Then Navigate to create “Nekustamie īpašumi” filter parameters page
+  And Leave all parameter fields empty
+  Then Press filter save button
+  Given Parameter page visible
+
+Scenario: “Nekustamie īpašumi” filter - positive
+  Given I am on intro screen
+  Then Navigate to create “Nekustamie īpašumi” filter parameters page
+  And Fill in filter Name (Nosaukums) and Price (Cena) fields with valid data
+  Then Press filter save button
+  Given Validate Filter page
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -21,16 +21,42 @@
 # For more on rspec assertions, check out
 # https://www.relishapp.com/rspec/rspec-expectations/docs
 
-
+# Uzd: 4.1 un 5.1
 Given /^I am on intro screen$/ do
     if $driver.find_elements(:xpath, '//android.widget.Button[contains(@text, "OK")]').size > 0
         $driver.find_element(:xpath, '//android.widget.Button[contains(@text, "OK")]').click
     end
     @screens.screen_intro.visible?
 end
+# Uzd: 4.2 un 5.2
+Then /^Navigate to create “Nekustamie īpašumi” filter parameters page$/ do
 
-
-Given /^I create property filter$/ do
-    @tests.test_add_filter.close_intro
-    @tests.test_add_filter.open_filter_parameter_screen
 end
+   
+# Given /^I create property filter$/ do
+#     @tests.test_add_filter.close_intro
+#     @tests.test_add_filter.open_filter_parameter_screen
+# end
+# Uzd: 4.3
+And /^Leave all parameter fields empty$/ do
+
+end
+# Uzd: 5.3
+And /^Fill in filter Name (Nosaukums) and Price (Cena) fields with valid data$/ do
+
+end
+   
+# Uzd: 4.4 un 5.4
+Then /^Press filter save button$/ do
+
+end
+   
+# Uzd: 4.5
+Given /^Parameter page visible$/ do
+
+end  
+# Uzd: 5.1 
+Given /^Validate Filter page$/ do
+
+end
+   
